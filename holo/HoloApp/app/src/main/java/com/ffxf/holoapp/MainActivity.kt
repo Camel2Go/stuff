1dc29002845b70de19c5469d40a35a7c04ab22ff
Camel2Go <art.2001@web.de>
2022-08-11 14:39:57 +0200

    idk

--- holo/HoloApp/app/src/main/java/com/ffxf/holoapp/MainActivity.kt
+++ holo/HoloApp/app/src/main/java/com/ffxf/holoapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ffxf.holoapp
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.os.PowerManager
 import android.util.Log
 import android.view.KeyEvent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 
@@ -15,39 +17,42 @@ class MainActivity : AppCompatActivity() {
     private val LOG_TAG: String = MainActivity::class.java.simpleName
     private val videoPath: String = "/Download/"
     private var videoFiles: Array<File> = emptyArray()
-    private var videoIndex = 0
-
+    private var videoIndex = 1
+    private var videoPlayerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            when (result.data?.extras?.get("keyCode")) {
+                KeyEvent.KEYCODE_POWER -> {
+                    startActivity(Intent("android.intent.action.ACTION_REQUEST_SHUTDOWN"));
+                    // PowerManagerService.shutdown()
+                    // This only works for system applications (signed with phone vendor key) with special permissions
+                }
+                KeyEvent.KEYCODE_VOLUME_UP -> {
+                    TODO("modulo ändern")
+                    videoIndex = (videoIndex + 1) % videoFiles.size
+                    playVideo()
+                }
+                KeyEvent.KEYCODE_VOLUME_DOWN -> {
+                    TODO("modulo ändern")
+                    videoIndex = (videoIndex - 1) % videoFiles.size
+                    playVideo()
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        videoFiles = File( Environment.getExternalStorageDirectory().path + videoPath).listFiles{ _, filename -> filename.endsWith("mp4")}
-        if (videoFiles.isEmpty()) {
+        this.videoFiles = File( Environment.getExternalStorageDirectory().path + videoPath).listFiles{ _, filename -> filename.endsWith("mp4")}
+        if (this.videoFiles.isEmpty()) {
             Log.d(LOG_TAG, "no videofiles present")
             return
         }
+        Log.d(LOG_TAG, videoFiles.size.toString())
         playVideo()
     }
     private fun playVideo() {
-        val videoPlaybackActivity = Intent(this, VideoPlayerActivity::class.java)
-        videoPlaybackActivity.putExtra("videoFile", videoFiles.get(videoIndex))
-        startActivity(videoPlaybackActivity)
-    }
-
-    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_POWER) {
-            startActivity(Intent("android.intent.action.ACTION_REQUEST_SHUTDOWN"));
-            // PowerManagerService.shutdown()
-            // This only works for system applications (signed with phone vendor key) with special permissions
-            return true
-        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            videoIndex++
-            playVideo()
-            return true
-        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-            videoIndex--
-            playVideo()
-            return true
-        }
-        return super.onKeyLongPress(keyCode, event)
+        val videoPlayerIntent = Intent(this, VideoPlayerActivity::class.java)
+        videoPlayerIntent.putExtra("videoFile", videoFiles[videoIndex])
+        videoPlayerLauncher.launch(videoPlayerIntent)
     }
 }
--- holo/HoloApp/app/src/main/java/com/ffxf/holoapp/VideoPlayerActivity.kt
+++ holo/HoloApp/app/src/main/java/com/ffxf/holoapp/VideoPlayerActivity.kt
@@ -1,12 +1,16 @@
 package com.ffxf.holoapp
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.media.AudioDescriptor
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.view.WindowInsets
 import android.widget.VideoView
@@ -16,40 +20,73 @@ import java.io.File
 
 class VideoPlayerActivity : Activity(), MediaPlayer.OnCompletionListener {
     private lateinit var mVV: VideoView
+    private lateinit var audioManager: AudioManager
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
+
+        // change orientation and visibility of navigation-bar
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         window.insetsController?.hide(WindowInsets.Type.navigationBars())
-        val videoFile = this.intent.extras?.get("videoFile")
+
+        val videoFile = this.intent.extras?.get("videoFile") as File
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         mVV = findViewById<View>(R.id.videoview) as VideoView
         mVV.setOnCompletionListener(this)
         mVV.setOnPreparedListener { mp -> mp.isLooping = true }
-        if (!this.playFile(videoFile as File)) {
-            return
-        }
+        mVV.setVideoURI(Uri.fromFile(videoFile))
         mVV.start()
     }
 
-    private fun playFile(videoFile: File): Boolean {
-        if (videoFile.exists() && videoFile.canRead()) {
-            mVV.setVideoURI(Uri.fromFile(videoFile))
-            return true
-        }
-        mVV.stopPlayback()
-        this.finish()
-        return false
-    }
+
 
     override fun onCompletion(p0: MediaPlayer?) {
         this.finish()
     }
+
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         setIntent(intent)
-        val videoFile = this.intent.extras?.get("videoPath")
-        this.playFile(videoFile as File)
+        val videoFile = this.intent.extras?.get("videoPath") as File
+        mVV.setVideoURI(Uri.fromFile(videoFile))
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_POWER ||keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            event.startTracking()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (!event.isCanceled) {
+            when (keyCode) {
+                KeyEvent.KEYCODE_VOLUME_UP -> audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_RAISE,
+                    AudioManager.FLAG_SHOW_UI
+                )
+                KeyEvent.KEYCODE_VOLUME_DOWN -> audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_LOWER,
+                    AudioManager.FLAG_SHOW_UI
+                )
+            }
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
+    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_POWER || keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            var keyCodeIntent = Intent()
+            keyCodeIntent.putExtra("keyCode", keyCode)
+            this.setResult(Activity.RESULT_OK, keyCodeIntent)
+            this.finish()
+            return true
+        }
+        return super.onKeyLongPress(keyCode, event)
     }
 }
